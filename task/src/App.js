a3e1841bb5349d95c97809cbae31f450acd81833
Shimil <shimiltharayil007@gmail.com>
2022-11-20 19:08:04 +0530

    Changes made

--- task/src/App.js
+++ task/src/App.js
@@ -1,14 +1,12 @@
-import {lazy,Suspense} from 'react'
-const Low= lazy(()=> import ('./Low'))
+import { Profile } from './Profile';
+
 
 function App() {
 
   return (
     <div className="m-10 lg:max-w-7xl mx-auto shadow-md items-center">
-     <Suspense fallback={<p>loading....</p>} >
-       <Low />
-     </Suspense>
-    </div>
+      <Profile />
+        </div>
   );
 }
 
